7151be58de192a0d0dd055c5e63b0fb13662a3c6
Kinnari Shah <kinnari1022@yahoo.com>
2020-09-21 01:47:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/' do
-   "My name is Kinnari!"
+  
+  get '/name' do
+    "My name is Kinnari!"
  end
+
+  get '/hometown' do
+     "My hometown is Lexington, Ky!"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is hanging tough"
+  end
+  
+  get '/' do
+    "helllo!"
+  end
 end
