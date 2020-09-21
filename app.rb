93eceef062b3cc195f7cb0f6a9e6cb7441e483f4
Wardayaz <wayaz107@gmail.com>
2020-09-21 18:18:35 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end 
+
+    get '/name' do
+        "My name is Warda"
+    end 
+
+    get '/hometown' do
+        "My hometown is Milwaukee"
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is 'Hotline Bling"
+    end 
 end
