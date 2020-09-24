1935ceb58da73d5789a1854eee7d3f32d20d9701
astanley74 <stanley.377@buckeyemail.osu.edu>
2020-09-24 15:15:37 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Austin"
+    end
+
+    get '/hometown' do
+        "My hometown is San Jose, CA"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is On Top"
+    end
 end
