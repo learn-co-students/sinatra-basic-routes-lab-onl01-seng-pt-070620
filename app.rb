bf273d10c39a3f9132f0c97b9baec423e5d269f1
jillbowen <jillian.b.short@gmail.com>
2020-09-23 18:50:33 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Jill"
+      end
+
+    get '/hometown' do
+        "My hometown is Shingletown"
+        end
+
+    get '/favorite-song' do
+        "My favorite song is ...that's a tough one..."
+        end
 end
