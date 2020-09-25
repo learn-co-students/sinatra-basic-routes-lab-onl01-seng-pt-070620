c1539874f0ed180cfbd6b4c6a4370943d0220eea
Denise Henderson <denise@henderson3.us>
2020-09-25 15:04:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Denise"
+  end
+
+  get '/hometown' do
+    "My hometown is Jacksonville"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Drift Away"
+  end
 end
